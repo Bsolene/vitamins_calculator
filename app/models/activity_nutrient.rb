04c710d7f1238585426f1a27c12fc71b5fbb72e5
Solene Basaia <solene.basaia@gmail.com>
2019-07-20 10:10:46 +0200

    fix typo

--- app/models/activity_nutrient.rb
+++ app/models/activity_nutrient.rb
@@ -1,7 +1,7 @@
 class ActivityNutrient < ApplicationRecord
-  belongs_to :activity_nutrient
+  belongs_to :activity
   belongs_to :nutrient
 
-  validates :activity_nutrient, presence: true
+  validates :activity, presence: true
   validates :nutrient, presence: true
 end
