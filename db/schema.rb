a2960ab9d97db7d1f459be19e3089a42d0c74b1d
Solene Basaia <solene.basaia@gmail.com>
2019-07-20 09:48:08 +0200

    create user model and add faker gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_073544) do
+ActiveRecord::Schema.define(version: 2019_07_20_074529) do
 
   create_table "nutrients", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.float "height"
+    t.float "weight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
